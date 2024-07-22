18b2c20570c247874d36808d9f28e69b8baf015e
Exequiel <exee.lp@hotmail.com>
2024-07-22 20:09:00 -0300

    cors de netfly

--- src/main/java/Podogonnet/App/config/security/HttpSecurityConfig.java
+++ src/main/java/Podogonnet/App/config/security/HttpSecurityConfig.java
@@ -86,7 +86,7 @@ public class HttpSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app/"));
+        configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
