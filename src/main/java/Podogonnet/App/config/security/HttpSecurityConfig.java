3628c55213af5050563cebb8cf135b6acad20a8a
Exequiel <exee.lp@hotmail.com>
2024-07-24 00:34:17 -0300

    cors de netfly agregue render

--- src/main/java/Podogonnet/App/config/security/HttpSecurityConfig.java
+++ src/main/java/Podogonnet/App/config/security/HttpSecurityConfig.java
@@ -86,7 +86,7 @@ public class HttpSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app","https://podofrontdeploy.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
@@ -94,4 +94,5 @@ public class HttpSecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
