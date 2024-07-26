95b17388313e3f267ec388754d24a8c8d5e5ab15
Exequiel <exee.lp@hotmail.com>
2024-07-26 12:52:20 -0300

    agregue rol para admin

---
+++ src/main/java/Podogonnet/App/servis/AdminService.java
@@ -0,0 +1,34 @@
+package Podogonnet.App.servis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import Podogonnet.App.entity.Usuario;
+import Podogonnet.App.enums.Rol;
+import Podogonnet.App.repository.UsuarioRepositorio;
+
+@Component
+public class AdminService implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (usuarioRepository.findByUserName("admin").isEmpty()) {
+            Usuario admin = new Usuario();
+            admin.setUserName("administrador");
+            admin.setNombre("Administrador");
+            admin.setEmail("admin@example.com");
+            admin.setPassword(passwordEncoder.encode("123456"));
+            admin.setRol(Rol.ADMIN);
+
+            usuarioRepository.save(admin);
+        }
+    }
+}
