498f09661cbe5a7b1d235a7f04716c3ea9168309
Exequiel <exee.lp@hotmail.com>
2024-07-29 16:03:03 -0300

    agregue localhost a los cors

--- src/main/java/Podogonnet/App/config/security/HttpSecurityConfig.java
+++ src/main/java/Podogonnet/App/config/security/HttpSecurityConfig.java
@@ -86,7 +86,7 @@ public class HttpSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app","https://podofrontdeploy.onrender.com"));
+        configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app","https://podofrontdeploy.onrender.com","http://localhost:5173/"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
