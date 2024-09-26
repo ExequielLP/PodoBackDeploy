8c5c39b04be783de825ed393628d5d7533245c54
Exequiel <exee.lp@hotmail.com>
2024-09-26 15:37:02 -0300

    Rebuild target

--- src/main/java/Podogonnet/App/util/CookieUtil.java
+++ src/main/java/Podogonnet/App/util/CookieUtil.java
@@ -2,7 +2,10 @@ package Podogonnet.App.util;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
 
 @Component
 public class CookieUtil {
@@ -10,16 +13,27 @@ public class CookieUtil {
 //        String environment = System.getenv("ENTORNO");
 //        boolean isProduction = "production".equalsIgnoreCase(environment);
 
-        Cookie cookie = new Cookie(name, value);
+//        Cookie cookie = new Cookie(name, value);
 //        if (isProduction) {
 //
 //        }
-        cookie.setSecure(true);
-        cookie.setHttpOnly(true);
-        cookie.setDomain(domein);
-        cookie.setMaxAge(MaxAge);
-        cookie.setPath("/");
-        httpResponse.addCookie(cookie);
+
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .domain(domein)
+                .path("/")
+                .maxAge(MaxAge)  // Establece el tiempo de expiración en segundos
+                .secure(true)    // Configura la cookie como segura
+                .httpOnly(true)  // Solo accesible mediante HTTP (no JavaScript)
+                .sameSite("None") // Define la política de SameSite
+                .build();
+
+        // Añade la cookie a la respuesta como un encabezado
+        httpResponse.addHeader("Set-Cookie", cookie.toString());
+//        cookie.setSecure(true);
+//        cookie.setHttpOnly(true);
+//        cookie.setDomain(domein);
+//        cookie.setMaxAge(MaxAge);
+//        cookie.setPath("/");
         System.out.println("en cookie util");
         System.out.println(cookie);
     }
