53ab722efd78560f65378677099ded4590d021d8
Exequiel <exee.lp@hotmail.com>
2024-09-27 15:38:25 -0300

    Rebuild target

--- src/main/java/Podogonnet/App/util/CookieUtil.java
+++ src/main/java/Podogonnet/App/util/CookieUtil.java
@@ -10,23 +10,23 @@ import java.time.Duration;
 @Component
 public class CookieUtil {
     public static void createCookie( HttpServletResponse httpResponse, String name, String value, String domein, Integer MaxAge) {
-        ResponseCookie cookie = ResponseCookie.from(name, value)
-                .domain(domein)
-                .path("/")
-                .maxAge(MaxAge)  // Establece el tiempo de expiración en segundos
-                .secure(true)    // Configura la cookie como segura
-                .httpOnly(true)  // Solo accesible mediante HTTP (no JavaScript)
-                .sameSite("None") // Define la política de SameSite
-                .build();
-        httpResponse.addHeader("Set-Cookie", cookie.toString());
-    //     Añade la cookie a la respuesta como un encabezado
-//        Cookie cookie = new Cookie(name, value);
-//       cookie.setSecure(true);
-//       cookie.setHttpOnly(true);
-//       cookie.setDomain(domein);
-//       cookie.setMaxAge(MaxAge);
-//       cookie.setPath("/");
-//       httpResponse.addCookie(cookie);
+//        ResponseCookie cookie = ResponseCookie.from(name, value)
+//                .domain(domein)
+//                .path("/")
+//                .maxAge(MaxAge)  // Establece el tiempo de expiración en segundos
+//                .secure(true)    // Configura la cookie como segura
+//                .httpOnly(true)  // Solo accesible mediante HTTP (no JavaScript)
+//                .sameSite("None") // Define la política de SameSite
+//                .build();
+//        httpResponse.addHeader("Set-Cookie", cookie.toString());
+
+        Cookie cookie = new Cookie(name, value);
+       cookie.setSecure(true);
+       cookie.setHttpOnly(true);
+       cookie.setDomain(domein);
+       cookie.setMaxAge(MaxAge);
+       cookie.setPath("/");
+       httpResponse.addCookie(cookie);
     }
     public static void clearCookie(HttpServletResponse httpServletResponse,String name){
         Cookie cookie=new Cookie(name,null);
