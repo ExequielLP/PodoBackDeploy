fb6161aae14cfd4461e3ba295dad8bd18345fb4e
Exequiel <exee.lp@hotmail.com>
2024-12-11 19:50:49 -0300

    Problamos colocar el setExposedHeader(serCokies) para ver si los dispositivos ios funcionaban correctamente

--- src/main/java/Podogonnet/App/controller/TurnoControlador.java
+++ src/main/java/Podogonnet/App/controller/TurnoControlador.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Collections;
@@ -37,32 +38,33 @@ public class TurnoControlador {
 
     @GetMapping("turnoDelDia/{date}")
     public ResponseEntity<List<TurnosUsuario>> listaDeTurnos(@PathVariable String date) throws Exception {
-      try {
-          LocalDate localDate = LocalDate.parse(date);
-          List<TurnosUsuario> dia = diaServicio.turnosDelDia(localDate);
-          return ResponseEntity.ok(dia);
-      }catch (Exception e){
-          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                  .body(Collections.emptyList()); // Manejo genérico de excepciones
-      }
+        try {
+            LocalDate localDate = LocalDate.parse(date);
+            List<TurnosUsuario> dia = diaServicio.turnosDelDia(localDate);
+            return ResponseEntity.ok(dia);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Collections.emptyList()); // Manejo genérico de excepciones
+        }
     }
 
     @PostMapping("/reservarTurno/{turnoId}/{servicioId}/{usuarioid}")
-    public ResponseEntity<Turno> bookAppointment(@PathVariable String turnoId, @PathVariable String servicioId ,@PathVariable String usuarioid ) {
+    public ResponseEntity<Turno> bookAppointment(@PathVariable String turnoId, @PathVariable String servicioId, @PathVariable String usuarioid) {
         System.out.println("ahi viene");
         System.out.println(usuarioid);
-        return ResponseEntity.ok(turnoServicio.reservarTurno(turnoId,servicioId,usuarioid));
+        return ResponseEntity.ok(turnoServicio.reservarTurno(turnoId, servicioId, usuarioid));
     }
 
     @GetMapping("/listaTurnos/{id}")
-    public ResponseEntity<List<TurnosUsuario>>listaTurno(@PathVariable String id) {
+    public ResponseEntity<List<TurnosUsuario>> listaTurno(@PathVariable String id) {
 
         return ResponseEntity.ok(turnoServicio.listaDeTurnosId(id));
 
     }
-    @GetMapping("/cancelarTurno/{id}")
-    public ResponseEntity<Turno> cancelarTurno(@PathVariable String id){
 
-        return ResponseEntity.ok( turnoServicio.cancelarTurno(id));
+    @GetMapping("/cancelarTurno/{id}")
+    public ResponseEntity<Turno> cancelarTurno(@PathVariable String id) {
+
+        return ResponseEntity.ok(turnoServicio.cancelarTurno(id));
     }
 }
--- src/main/java/Podogonnet/App/repository/TurnoRepository.java
+++ src/main/java/Podogonnet/App/repository/TurnoRepository.java
@@ -10,8 +10,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,17 +28,69 @@ public interface TurnoRepository extends JpaRepository<Turno, String> {
 
     boolean existsByStartTimeAndEndTime(LocalDateTime startTime, LocalDateTime endTime);
 
-   // Optional<List<Turno>> findByDia();
-   @Query(value = "SELECT t.* FROM turno t JOIN dia d ON t.dia_id = d.id WHERE d.fecha = :diaFecha", nativeQuery = true)
-   List<Turno> findTurnosByDiaFecha(@Param("diaFecha") LocalDate diaFecha);
+    // Optional<List<Turno>> findByDia();
+    @Query(value = "SELECT t.* FROM turno t JOIN dia d ON t.dia_id = d.id WHERE d.fecha = :diaFecha", nativeQuery = true)
+    List<Turno> findTurnosByDiaFecha(@Param("diaFecha") LocalDate diaFecha);
 
 
     Page<Turno> findByEstadoTrue(Pageable pageable);
 
     List<Turno> findByStartTimeBetween(LocalDateTime startOfMonth, LocalDateTime endOfMonth);
-
-
-    List<Turno> findByUsuarioNombre(String nombre);
+//
+//    @Query(value = "SELECT * FROM turno t WHERE t.usuario_nombre LIKE %:nombre%", nativeQuery = true)
+//    List<Turno> findByUsuarioNombre(String nombre);
 
     List<Turno> findByServicioPodoNombre(String servicio);
+
+
+// QUERY NO NATIVA QUE FUNCIONA
+//        @Query("SELECT t FROM Turno t " +
+//            "JOIN t.servicioPodo s " +
+//            "JOIN t.usuario u " +
+//            "JOIN t.dia d " +
+//            "WHERE (:servicio IS NULL OR s.nombre LIKE %:servicio%) " +
+//            "AND (:nombre IS NULL OR u.nombre LIKE %:nombre%) " +
+//            "AND (:fecha IS NULL OR d.fecha = :fecha)")
+//    List<Turno> searchByCriteria(@Param("nombre") String nombre,
+//                                 @Param("servicio") String servicio,
+//                                 @Param("fecha") LocalDate fecha);
+// QUERY NATIVA
+
+
+
+        @Query(value = "SELECT t.* FROM turno t " +
+                "JOIN servicio_podo s ON t.servicio_podo_id = s.id " +
+                "JOIN usuario u ON t.usuario_id = u.id " +
+                "JOIN dia d ON t.dia_id = d.id " +
+                "WHERE (:servicio IS NULL OR s.nombre LIKE CONCAT('%', :servicio, '%')) " +
+                "AND (:nombre IS NULL OR u.nombre LIKE CONCAT('%', :nombre, '%')) " +
+                "AND (:fecha IS NULL OR d.fecha = :fecha)",
+                nativeQuery = true)
+        List<Turno> searchByCriteria(@Param("nombre") String nombre,
+                                     @Param("servicio") String servicio,
+                                     @Param("fecha") LocalDate fecha);
+
+
+//    @Query("SELECT t FROM Turno t " +
+//            "JOIN t.servicioPodo s " +
+//            "JOIN t.usuario u " +
+//            "JOIN t.dia d " +
+//            "WHERE (:servicio IS NULL OR s.nombre LIKE %:servicio%) " +
+//            "AND (:fecha IS NULL OR d.fecha = :fecha)" +
+//            "AND (:name IS NULL OR u.nombre LIKE %:name%) ")
+//    List<Turno> searchByCriteria(@Param("name") String name,
+//                                 @Param("servicio") String servicio,
+//                                 @Param("fecha") java.sql.Date fecha);
+    
+
 }
+
+
+
+
+
+
+
+
+
+
