123a26777e9864c1368228518cf439001f529ef9
Exequiel <exee.lp@hotmail.com>
2024-12-13 17:42:01 -0300

    Vemos que sigue sin funcionar

--- src/main/java/Podogonnet/App/servis/auth/AutheticateService.java
+++ src/main/java/Podogonnet/App/servis/auth/AutheticateService.java
@@ -86,6 +86,7 @@ public class AutheticateService {
             domein="localhost";
             System.out.println(domein);
         }else {domein=".onrender.com";
+            System.out.println("------------------+++++++--------------++++++++++---------------+++++++++");
             System.out.println(domein);
         }
 
